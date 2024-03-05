eef2ba389be21d5a1f925851696e36c1438474ba
mkaocodes <mkaoibrahim@gmail.com>
2024-03-05 10:39:38 +0300

    Color Code Change

--- app/src/main/java/com/example/haofiti/ui/theme/Color.kt
+++ app/src/main/java/com/example/haofiti/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.haofiti.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF5D432C)
+val PurpleGrey80 = Color(0xFFD2B48C)
+val Pink80 = Color(0xFFF5E9CC)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF8B715A)
+val PurpleGrey40 = Color(0xFFE6D7B8)
+val Pink40 = Color(0xFFFDF7E8)
