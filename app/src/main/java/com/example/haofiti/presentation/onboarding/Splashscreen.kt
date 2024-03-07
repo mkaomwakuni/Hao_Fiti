2a70eee904345bb71ccf0d55157326294c3d8b0c
mkaocodes <mkaoibrahim@gmail.com>
2024-03-07 19:23:21 +0300

    Splashscreen UI added

--- app/src/main/java/com/example/haofiti/presentation/onboarding/Splashscreen.kt
+++ app/src/main/java/com/example/haofiti/presentation/onboarding/Splashscreen.kt
@@ -1,28 +1,27 @@
 package com.example.haofiti.presentation.onboarding
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,39 +33,38 @@ import com.example.haofiti.R
 @Composable
 fun SplashScreen() {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
         ) {
             Box(
                 modifier = Modifier
-                    .height(335.dp)
+                    .height(393.dp)
                     .fillMaxWidth()
                     .clip(shape = RoundedCornerShape(bottomStart = 60.dp))
-                    .background(color = Color.White)
+                    .background(color = Color.Transparent)
             ) {
                 Image(
                     modifier = Modifier
                         .fillMaxSize(),
-                    painter = painterResource(id = R.drawable.kino),
-                    contentDescription = "banner"
-                )
+                    painter = painterResource(id =  R.drawable.kino),
+                    contentDescription = "banner"  )
             }
 
-            Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(30.dp))
 
-            Texts(text = "Find Your\nIdeal Home")
+            Texts(textResId = R.string.splash_screen_title)
 
             Spacer(modifier = Modifier.height(5.dp))
 
-            TextSubs(text = "Just within clicks you can search\nand find your Dream Home and stay")
+            TextSubs(textResId = R.string.splash_screen_subtitle)
 
-            Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
-            TextButtonSplash(text = "Get Started", onClick = {})
+            TextButtonSplash(textResId = R.string.get_started_button, onClick = {})
         }
     }
 }
@@ -74,58 +72,55 @@ fun SplashScreen() {
 
 @Composable
 fun Texts(
-    text: String,
+    @StringRes textResId: Int,
     modifier: Modifier = Modifier,
-    color: Color = Color.Black,
     fontWeight: FontWeight? = FontWeight.Bold,
+    fontFamily: FontFamily? = FontFamily.Default,
     textAlign: TextAlign? = TextAlign.Start
 ) {
     Text(
-        modifier = modifier
-            .padding(20.dp),
-        text = text,
-        fontSize = 40.sp,
-        color = color,
+        modifier = modifier.padding(20.dp),
+        text = stringResource(id = textResId),
+        fontSize = 50.sp,
         fontWeight = fontWeight,
+        fontFamily = fontFamily,
         textAlign = textAlign
     )
 }
+
 @Composable
 fun TextSubs(
-    text: String,
+    @StringRes textResId: Int,
     modifier: Modifier = Modifier,
-    color: Color = Color.Black,
+    fontFamily: FontFamily? = FontFamily.Cursive,
     fontWeight: FontWeight? = FontWeight.Normal,
     textAlign: TextAlign? = TextAlign.Start
 ) {
     Text(
-        modifier = modifier
-            .padding(20.dp),
-        text = text,
+        modifier = modifier.padding(20.dp),
+        text = stringResource(id = textResId),
         fontSize = 20.sp,
-        color = color,
         fontWeight = fontWeight,
+        fontFamily = fontFamily,
         textAlign = textAlign
     )
 }
+
 @Composable
 fun TextButtonSplash(
-    text: String,
-    onClick: () -> Unit
-) {
+    @StringRes textResId: Int,
+    onClick: () -> Unit) {
     TextButton(
         onClick = onClick,
         modifier = Modifier
-                .fillMaxWidth()
+            .fillMaxWidth()
             .height(100.dp)
             .padding(20.dp)
-            .clip(shape = RoundedCornerShape(10.dp))
-            .background(Color(0xFF2FAB6B), shape = RoundedCornerShape(10.dp))
-
-
+            .clip(shape = RoundedCornerShape(20.dp))
+            .background(color = Color.Gray, shape = RoundedCornerShape(20.dp))
     ) {
         Text(
-            text = text,
+            text = stringResource(id = textResId),
             fontSize = 18.sp,
             color = Color.White,
             fontWeight = FontWeight.Bold,
@@ -135,7 +130,7 @@ fun TextButtonSplash(
 }
 
 
-@Preview()
+@Preview(showBackground = true)
 @Composable
 fun PreviewSplashScreen() {
     SplashScreen()
