166b62279cbc0ccdd9dcd54fd8ffcd189b00e397
mkaocodes <mkaoibrahim@gmail.com>
2024-07-17 17:35:41 +0300

    - Refactor

---
+++ app/src/main/java/iz/housing/haofiti/ui/theme/presentation/maps/MapsExplorer.kt
@@ -0,0 +1,23 @@
+package iz.housing.haofiti.ui.theme.presentation.maps
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import iz.housing.haofiti.ui.theme.presentation.common.BottomNavComponent
+
+@Composable
+fun MapExplorer(navController: NavController) {
+    Scaffold(
+        bottomBar = { BottomNavComponent(navController = navController) }
+    ) { paddingValues ->
+        Column(modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()) {
+
+        }
+    }
+}
