dff885af04f1e1701639bb0961e63df9951ebd53
mkaocodes <mkaoibrahim@gmail.com>
2024-07-19 17:41:01 +0300

    - Fetching results and adjustments on the layout

--- app/src/main/java/iz/housing/haofiti/ui/theme/presentation/home/HomePage.kt
+++ app/src/main/java/iz/housing/haofiti/ui/theme/presentation/home/HomePage.kt
@@ -1,6 +1,5 @@
 package iz.housing.haofiti.ui.theme.presentation.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,8 +14,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -28,7 +29,9 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,48 +41,58 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import iz.housing.haofiti.R
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import iz.housing.haofiti.data.model.PropertyItem
+import iz.housing.haofiti.ui.theme.presentation.common.BottomNavComponent
+import iz.housing.haofiti.viewmodels.HouseViewModel
 
 
 @Composable
-fun UrbanHomeFinder() {
+fun HomePage(viewModel: HouseViewModel,navController: NavController) {
+    val scroll = rememberScrollState()
     var searchQuery by remember { mutableStateOf("") }
+    val properties by viewModel.properties.collectAsState()
+    val location by viewModel.locations.collectAsState()
+    Scaffold(
+        bottomBar = {BottomNavComponent(navController = navController)},
+    ) { paddingValue ->
+        Column(modifier = Modifier.fillMaxSize().padding(paddingValue).verticalScroll(scroll)) {
+            val annotedText = buildAnnotatedString {
+                pushStyle(SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+                append("Let's find your\n")
+                pushStyle(SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+                append("dream home.")
+                addStyle(SpanStyle(color = MaterialTheme.colorScheme.primary), 15, 26)
+                pop()
+            }
 
-        val annotedText = buildAnnotatedString {
-            pushStyle(SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
-            append("Let's find your\n")
-            pushStyle(SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
-            append("dream home.")
-            addStyle(SpanStyle(color = MaterialTheme.colorScheme.primary), 15, 26)
-            pop()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .padding(start = 16.dp, end = 16.dp, top = 30.dp)
+            ) {
+                Text(
+                    text = "urban.",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                BasicText(text = annotedText)
+                Spacer(modifier = Modifier.height(16.dp))
+                SearchBar1(searchQuery = searchQuery, onSearchQueryChange = { searchQuery = it })
+                Spacer(modifier = Modifier.height(16.dp))
+                PreviouslyViewedSection(properties)
+                Spacer(modifier = Modifier.height(16.dp))
+                RecommendationsSection(properties)
+            }
         }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(start = 16.dp, end = 16.dp, top = 30.dp)
-    ) {
-        Text(
-            text = "urban.",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        BasicText(text = annotedText)
-        Spacer(modifier = Modifier.height(16.dp))
-        SearchBar1(searchQuery = searchQuery, onSearchQueryChange = { searchQuery = it })
-        Spacer(modifier = Modifier.height(16.dp))
-        PreviouslyViewedSection()
-        Spacer(modifier = Modifier.height(16.dp))
-        RecommendationsSection()
     }
 }
 
@@ -101,7 +114,7 @@ fun SearchBar1(searchQuery: String, onSearchQueryChange: (String) -> Unit) {
 }
 
 @Composable
-fun PreviouslyViewedSection() {
+fun PreviouslyViewedSection(properties: List<PropertyItem>) {
     Column {
         Text(
             text = "Previously Viewed",
@@ -112,7 +125,7 @@ fun PreviouslyViewedSection() {
         LazyRow(
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(getPreviouslyViewedProperties()) { property ->
+            items(properties.take(2)) { property ->
                 PropertyCardHorizontal(property)
             }
         }
@@ -120,7 +133,7 @@ fun PreviouslyViewedSection() {
 }
 
 @Composable
-fun RecommendationsSection() {
+fun RecommendationsSection(properties: List<PropertyItem>) {
     Column {
         Text(
             text = "Our Recommendations",
@@ -129,7 +142,7 @@ fun RecommendationsSection() {
         )
         Spacer(modifier = Modifier.height(8.dp))
         Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            getRecommendedProperties().forEach { property ->
+            properties.forEach{ property ->
                 PropertyCardVertical(property)
             }
         }
@@ -138,7 +151,7 @@ fun RecommendationsSection() {
 
 
 @Composable
-fun PropertyCardHorizontal(property: Property2) {
+fun PropertyCardHorizontal(property: PropertyItem) {
     Card(
         modifier = Modifier
             .width(280.dp)
@@ -147,8 +160,8 @@ fun PropertyCardHorizontal(property: Property2) {
     ) {
         Column {
             Box {
-                Image(
-                    painter = painterResource(id = property.imageRes),
+                AsyncImage(
+                    model = property.images.firstOrNull(),
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -172,14 +185,14 @@ fun PropertyCardHorizontal(property: Property2) {
                     Text(text = property.location, color = Color.Gray, fontSize = 14.sp)
                 }
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = property.price, color = MaterialTheme.colorScheme.primary)
+                Text(text = "Ksh ${property.id}/month", color = MaterialTheme.colorScheme.primary)
             }
         }
     }
 }
 
 @Composable
-fun PropertyCardVertical(property: Property2) {
+fun PropertyCardVertical(property: PropertyItem) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -190,8 +203,8 @@ fun PropertyCardVertical(property: Property2) {
             Box(modifier = Modifier
                 .fillMaxWidth()
                 .height(160.dp)) {
-                Image(
-                    painter = painterResource(id = property.imageRes),
+                AsyncImage(
+                    model = property.images.firstOrNull(),
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -218,7 +231,7 @@ fun PropertyCardVertical(property: Property2) {
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(text = property.name, fontWeight = FontWeight.Bold)
-                    Text(text = property.price,fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
+                    Text(text = "Ksh ${property.id}/month",fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
                 }
                 Spacer(modifier = Modifier.height(6.dp))
                 Row(modifier = Modifier.fillMaxWidth()) {
@@ -230,28 +243,3 @@ fun PropertyCardVertical(property: Property2) {
     }
 }
 
-data class Property2(
-    val name: String,
-    val location: String,
-    val price: String,
-    val imageRes: Int
-)
-
-fun getPreviouslyViewedProperties(): List<Property2> {
-    return listOf(
-        Property2("Treasure Cove", "Miami Beach, FL", "£50,000/month", R.drawable.kiambu),
-        Property2("Cozy Barriada", "Barcelona, Spain", "£55,000/month", R.drawable.lavington)
-    )
-}
-
-fun getRecommendedProperties(): List<Property2> {
-    return listOf(
-        Property2("The White Abode", "Santorini, Greece", "£60,000/month", R.drawable.westlands),
-        Property2("Urban Loft", "New York City, NY", "£65,000/month", R.drawable.ngara)
-    )
-}
-@Preview
-@Composable
-fun UrbanHomeFinderPreview() {
-    UrbanHomeFinder()
-}
