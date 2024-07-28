24c99c1f1177046715799c9313c84d2523eccca7
mkaocodes <mkaoibrahim@gmail.com>
2024-07-28 13:35:17 +0300

    - Implemented Google Maps view in MapExplorer screen. - Added map initialization and marker setup. - Updated MapFocused screen to include BarEffect and MapExplorer - Removed bottom navigation from MapExplorer and added it to MapFocused. - Added Google Maps API key to AndroidManifest.xml. - Updated HomeDetail screen data file. - Updated HouseNavGraph to navigate to MapFocused  Removed unused imports and variables in HouseNavGraph. - Removed unnecessary annotation in MainActivity. - Removed unused import in build.gradle.

--- app/src/main/java/iz/housing/haofiti/MainActivity.kt
+++ app/src/main/java/iz/housing/haofiti/MainActivity.kt
@@ -1,9 +1,9 @@
 package iz.housing.haofiti
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +12,7 @@ import iz.housing.haofiti.ui.theme.presentation.navigation.HouseNavGraph
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
