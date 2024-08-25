a6a672ef537e55ab206d9c5eaee52940057aecd8
mkaocodes <mkaoibrahim@gmail.com>
2024-08-25 20:01:57 +0300

    -fix

--- app/src/main/java/iz/housing/haofiti/ui/theme/Theme.kt
+++ app/src/main/java/iz/housing/haofiti/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package iz.housing.haofiti.ui.theme
+package iz.housing.haofiti.ui.theme.presentation.common
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -9,9 +9,15 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import iz.housing.haofiti.ui.theme.Pink40
+import iz.housing.haofiti.ui.theme.Pink80
+import iz.housing.haofiti.ui.theme.Purple40
+import iz.housing.haofiti.ui.theme.Purple80
+import iz.housing.haofiti.ui.theme.PurpleGrey40
+import iz.housing.haofiti.ui.theme.PurpleGrey80
+import iz.housing.haofiti.ui.theme.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -28,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun HaoFitiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -36,15 +42,15 @@ fun HaoFitiTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> LightColorScheme
+        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
     val systemUiController = rememberSystemUiController()
     SideEffect {
         systemUiController.setSystemBarsColor(
-            color = Color.White,
-            darkIcons = darkTheme
+            color = colorScheme.surface,
+            darkIcons = !darkTheme
         )
     }
 
--- app/src/main/java/iz/housing/haofiti/ui/theme/presentation/navigation/HouseNavGraph.kt
+++ app/src/main/java/iz/housing/haofiti/ui/theme/presentation/navigation/HouseNavGraph.kt
@@ -2,7 +2,7 @@ package iz.housing.haofiti.ui.theme.presentation.navigation
 
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -26,10 +26,7 @@ import iz.housing.haofiti.viewmodels.HouseViewModel
 fun HouseNavGraph(navController: NavHostController) {
     val houseViewModel: HouseViewModel = hiltViewModel()
     NavHost(navController = navController, startDestination = Route.Home.route) {
-        composable(
-            enterTransition = { fadeIn(animationSpec = tween(1000)) },
-            exitTransition = { fadeOut(animationSpec = tween(1000)) },
-            route = Route.Home.route) {
+        composable(route = Route.Home.route) {
             HomePage(
                 navController = navController,
                 state = houseViewModel.uiState.collectAsState().value,
@@ -60,8 +57,8 @@ fun HouseNavGraph(navController: NavHostController) {
         }
         composable(
             route = Route.Explore.route,
-            enterTransition = { fadeIn(animationSpec = tween(1000)) },
-            exitTransition = { fadeOut(animationSpec = tween(1000)) }) {
+            enterTransition = { slideInHorizontally(animationSpec = tween(1000)) },
+            exitTransition = { slideOutHorizontally (animationSpec = tween(1000)) }) {
             Discovery(
                 navController = navController,
                 state = houseViewModel.uiState.collectAsState().value,
