bd4ededebed5e9a78a4b3d58789a6d6abc4c200e
mkaocodes <mkaoibrahim@gmail.com>
2024-08-29 18:18:30 +0300

    - Code documentation

--- app/src/main/java/iz/housing/haofiti/data/di/AppModule.kt
+++ app/src/main/java/iz/housing/haofiti/data/di/AppModule.kt
@@ -15,10 +15,21 @@ import iz.housing.haofiti.data.database.HouseTypeConvertors
 import iz.housing.haofiti.data.repository.HouseRepository
 import javax.inject.Singleton
 
+/**
+ * Dagger Hilt module for providing the HouseRepository dependency.
+ * This module is installed in the SingletonComponent, ensuring app-wide singleton instances.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class AppModule {
 
+    /**
+     * Binds the HouseRepositoryImpl to the HouseRepository interface.
+     * This allows Hilt to inject HouseRepository where needed, using the HouseRepositoryImpl implementation.
+     *
+     * @param houseRepositoryImpl The implementation of HouseRepository to be used.
+     * @return An instance of HouseRepository, which is actually a HouseRepositoryImpl.
+     */
     @Binds
     @Singleton
     abstract fun bindHouseRepository(
@@ -26,20 +37,40 @@ abstract class AppModule {
     ): HouseRepository
 }
 
+/**
+ * Dagger Hilt module for providing Firebase-related dependencies.
+ * This module is installed in the SingletonComponent, ensuring app-wide singleton instances.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 object FirebaseModule {
 
+    /**
+     * Provides a singleton instance of FirebaseDatabase.
+     *
+     * @return The FirebaseDatabase instance.
+     */
     @Provides
     @Singleton
     fun provideFirebaseDatabase(): FirebaseDatabase {
         return FirebaseDatabase.getInstance()
     }
 }
+
+/**
+ * Dagger Hilt module for providing database-related dependencies.
+ * This module is installed in the SingletonComponent, ensuring app-wide singleton instances.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseModule{
+object DatabaseModule {
 
+    /**
+     * Provides a singleton instance of HouseDatabase.
+     *
+     * @param application The Application instance, used as context for the database.
+     * @return A Room database instance of HouseDatabase.
+     */
     @Provides
     @Singleton
     fun providesHouseDatabase(application: Application): HouseDatabase {
@@ -52,6 +83,12 @@ object DatabaseModule{
             .build()
     }
 
+    /**
+     * Provides a singleton instance of HouseDao.
+     *
+     * @param houseDatabase The HouseDatabase instance from which to get the DAO.
+     * @return The HouseDao instance.
+     */
     @Provides
     @Singleton
     fun providesDao(houseDatabase: HouseDatabase): HouseDao = houseDatabase.houseDao()
